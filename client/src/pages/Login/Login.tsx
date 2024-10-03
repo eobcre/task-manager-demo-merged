42a7205a2989fc629fd44035aa6d0aa17e430069
Emmy Manning <emswoo@EM.local>
2024-10-03 15:15:40 -0400

    improved login UI

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -1,38 +1,36 @@
 import { Icon } from '@iconify/react';
 import { loginData } from '../../data/loginData';
+import Button from '../../components/Button';
 
 const Login = () => {
   return (
-    <div className='flex justify-center items-center w-full h-screen'>
-      <div className='flex flex-col items-center gap-6'>
-        <Icon icon='fluent-emoji-high-contrast:raccoon' className='home-icon text-purple w-[60px] h-[60px]' />
-        <p>Log in with the username and passcode below and assign the task to yourself.</p>
-        <div className='flex gap-7'>
-          {loginData.map((login) => (
-            <fieldset key={login.id} className='border-2 border-gray-300 p-4 rounded'>
-              <legend className='text-lg font-semibold px-2'>{login.accountName}</legend>
-              <p>
-                username:<span className='font-bold ml-2'>{login.userName}</span>
-              </p>
-              <p>
-                passcode:<span className='font-bold ml-2'>{login.passcode}</span>
-              </p>
-            </fieldset>
-          ))}
-        </div>
-        <div className='flex flex-col gap-4 mt-7'>
-          <div>
-            <label htmlFor='' className=''>
-              username
-            </label>
-            <input type='text' className='border border-slate-400 rounded-sm ml-4 p-1 w-60' />
+    <div className='flex justify-center items-center h-screen'>
+      <div className='w-[440px]'>
+        <div className='flex flex-col items-center gap-6'>
+          <Icon icon='fluent-emoji-high-contrast:raccoon' className='home-icon text-purple w-[60px] h-[60px]' />
+          <p>Log in with the username and passcode below and assign the task to another user.</p>
+          <div className='flex justify-center items-center gap-7'>
+            {loginData.map((login) => (
+              <fieldset key={login.id} className='border-2 border-gray-300 p-4 rounded'>
+                <legend className='text-lg font-semibold px-2'>{login.accountName}</legend>
+                <p>
+                  username:<span className='font-bold ml-2'>{login.userName}</span>
+                </p>
+                <p>
+                  passcode:<span className='font-bold ml-2'>{login.passcode}</span>
+                </p>
+              </fieldset>
+            ))}
           </div>
-          <div>
-            <label htmlFor='' className=''>
-              passcode
-            </label>
-            <input type='password' className='border border-slate-400 rounded-sm ml-[1.2rem] p-1 w-60' />
+          <div className='flex flex-col gap-4 mt-7 w-[440px]'>
+            <div>
+              <input type='text' placeholder='username' className='border border-slate-400 rounded-sm p-2 w-full' />
+            </div>
+            <div>
+              <input type='password' placeholder='passcode' className='border border-slate-400 rounded-sm p-2 w-full' />
+            </div>
           </div>
+          <Button buttonName='Log in' buttonStyle='text-white bg-purple hover:opacity-90 rounded-md px-6 py-2 w-full' />
         </div>
       </div>
     </div>
