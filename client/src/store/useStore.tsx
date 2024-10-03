910b5435f9ba1e62c1b9512e90cea08bf4dfc593
Emmy Manning <emswoo@EM.local>
2024-10-03 13:28:45 -0400

    login

--- client/src/store/useStore.tsx
+++ client/src/store/useStore.tsx
@@ -2,12 +2,16 @@ import { create } from 'zustand';
 
 interface StoreProps {
   isNavOpen: boolean;
+  auth: boolean;
   setNavOpen: (state: boolean) => void;
+  setAuth: (state: boolean) => void;
 }
 
 const useStore = create<StoreProps>((set) => ({
   isNavOpen: false,
+  auth: false,
   setNavOpen: (state) => set({ isNavOpen: state }),
+  setAuth: (state) => set({ auth: state }),
 }));
 
 export default useStore;
