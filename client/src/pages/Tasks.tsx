06f2fcc2ff82a387c7bccac6eea2462cfc2277e3
Emmy Manning <emswoo@EM.local>
2024-10-03 11:45:35 -0400

    fetching data

--- client/src/pages/Tasks.tsx
+++ client/src/pages/Tasks.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import ServerClient from '../services/ServerClient';
 import Grid from '../components/Grid';
 import Tab from '../components/Tab';
 import Button from '../components/Button';
@@ -7,13 +8,59 @@ import AssignTask from './AssignTask';
 const Tasks = () => {
   const [activeTab, setActiveTab] = useState('My Tasks');
   const [assignTaskOpen, setAssignTaskOpen] = useState(false);
+  const [taskData, setTaskData] = useState([]);
+  const [selectedTask, setSelectedTask] = useState('');
+  const [selectedDocumentType, setSelectedDocumentType] = useState('');
+  const [description, setDescription] = useState('');
 
+  useEffect(() => {
+    const fetchTaskData = async () => {
+      try {
+        const serverClient = new ServerClient('/api/retrieveTasks');
+        const res = await serverClient.get({});
+        // console.log('res', res.data);
+        setTaskData(res.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+    fetchTaskData();
+  }, []);
+
+  // assign task button
   const handleClickAssignTask = () => {
     setAssignTaskOpen(true);
   };
 
-  const onSubmit = () => {
-    console.log('Submitted!!!');
+  // submission
+  const onSubmit = async () => {
+    try {
+      const serverClient = new ServerClient('/api/assignTasks');
+      const res = await serverClient.post({ taskName: selectedTask, documentType: selectedDocumentType, description });
+      // console.log('res', res.data);
+      setAssignTaskOpen(false);
+      setSelectedTask('');
+      setSelectedDocumentType('');
+      setDescription('');
+    } catch (error) {
+      console.error('Error', error);
+    }
+  };
+
+  // task radio
+  const handleChangeTask = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // console.log('handleChangeTask', e.target.value);
+    setSelectedTask(e.target.value);
+  };
+  // document type radio
+  const handleChangeDocumentType = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // console.log('handleChangeDocumentType', e.target.value);
+    setSelectedDocumentType(e.target.value);
+  };
+  // description
+  const handleChangeDesc = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    // console.log('handleChangeDescription', e.target.value);
+    setDescription(e.target.value);
   };
 
   return (
@@ -24,9 +71,19 @@ const Tasks = () => {
           <Button buttonName='Assign Task' buttonStyle='text-white bg-purple hover:opacity-90 rounded-md px-6 py-2' buttonClick={handleClickAssignTask} />
         </div>
         <Tab activeTab={activeTab} setActiveTab={setActiveTab} />
-        {activeTab === 'My Tasks' && <Grid />}
+        {activeTab === 'My Tasks' && <Grid taskData={taskData} />}
       </div>
-      {assignTaskOpen && <AssignTask onSubmit={onSubmit} />}
+      {assignTaskOpen && (
+        <AssignTask
+          onSubmit={onSubmit}
+          selectedTask={selectedTask}
+          description={description}
+          selectedDocumentType={selectedDocumentType}
+          handleChangeTask={handleChangeTask}
+          handleChangeDocumentType={handleChangeDocumentType}
+          handleChangeDesc={handleChangeDesc}
+        />
+      )}
     </div>
   );
 };
