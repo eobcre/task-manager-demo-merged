e2e8b2017608e89f67d855615af6a75fded9ddb2
Emmy Manning <emswoo@EM.local>
2024-10-03 11:39:52 -0400

    assign tasks, retrieve tasks API

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -1,12 +1,31 @@
 const express = require('express');
 const router = express.Router();
-const Comment = require('../models/tasks');
+const AssignTask = require('../models/assignTask');
+
+router.post('/assignTasks', async (req, res) => {
+  const { taskName, documentType, description } = req.body;
+
+  const newTask = new AssignTask({
+    taskName,
+    documentType,
+    description,
+  });
+
+  try {
+    const savedTask = await newTask.save();
+    res.status(201).json(savedTask);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 router.get('/retrieveTasks', async (req, res) => {
   try {
-    const comments = await Comment.find();
-    res.json(comments);
+    const tasks = await AssignTask.find();
+    // console.log('tasks', tasks);
+    res.json(tasks);
   } catch (error) {
+    console.log('Error retrieving tasks:', error.message);
     res.status(500).json({ message: error.message });
   }
 });
