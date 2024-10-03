234ccb121b305c6f02246eb6b738d6d8a7188f83
Emmy Manning <emswoo@EM.local>
2024-10-03 13:28:26 -0400

    login route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,27 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import useStore from './store/useStore';
+import Login from './pages/Login/Login';
 import Nav from './components/Nav';
 import Tasks from './pages/Tasks';
 import Settings from './pages/Settings';
 
 const App = () => {
-  const { isNavOpen } = useStore();
+  const { isNavOpen, auth } = useStore();
 
   return (
-    <div className='flex'>
-      <Nav />
-      <main className={`bg-lightGray w-full ${isNavOpen ? '330px' : '65px'}`}>
+    <div className={auth ? 'flex' : ''}>
+      {auth && <Nav />}
+      <main className={`${auth ? `bg-lightGray w-full ${isNavOpen ? '330px' : '65px'}` : ''}`}>
         <Routes>
-          <Route index element={<Tasks />} />
-          <Route path='/Tasks' element={<Tasks />} />
-          <Route path='/Settings' element={<Settings />} />
+          {!auth ? (
+            <Route path='*' element={<Login />} />
+          ) : (
+            <>
+              <Route path='*' element={<Navigate to='/Tasks' />} />
+              <Route path='/Tasks' element={<Tasks />} />
+              <Route path='/Settings' element={<Settings />} />
+            </>
+          )}
         </Routes>
       </main>
     </div>
