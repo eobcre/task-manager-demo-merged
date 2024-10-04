10dbced8c20110f1792bab3230a8a0bf4690012a
Emmy Manning <emswoo@EM.local>
2024-10-04 19:49:57 -0400

    updated userId and flag

--- client/src/store/useLoginStore.tsx
+++ client/src/store/useLoginStore.tsx
@@ -3,10 +3,14 @@ import { persist } from 'zustand/middleware';
 
 interface LoginStoreProps {
   userLogin: boolean;
+  userId: number | null;
   userName: string;
+  flag: boolean;
 
   setUserLogin: (state: boolean) => void;
+  setUserId: (id: number | null) => void;
   setUserName: (name: string) => void;
+  setFlag: (flag: boolean) => void;
   UserLogout: () => void;
 }
 
@@ -14,15 +18,21 @@ const useStore = create<LoginStoreProps>()(
   persist(
     (set) => ({
       userLogin: false,
+      userId: null,
       userName: '',
+      flag: false,
 
       setUserLogin: (state) => set({ userLogin: state }),
+      setUserId: (id) => set({ userId: id }),
       setUserName: (name) => set({ userName: name }),
+      setFlag: (flag) => set({ flag: flag }),
 
       UserLogout: () => {
         set(() => ({
           userLogin: false,
+          userId: null,
           userName: '',
+          flag: false,
         }));
         localStorage.removeItem('auth-storage');
         localStorage.removeItem('token');
