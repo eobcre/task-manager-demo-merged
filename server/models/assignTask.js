45da22258574571db0bcb9269b6a081dc63d83c9
Emmy Manning <emswoo@EM.local>
2024-10-04 19:55:30 -0400

    updated userId, userName, assignTo

--- server/models/assignTask.js
+++ server/models/assignTask.js
@@ -2,8 +2,14 @@ const mongoose = require('mongoose');
 
 const assignTaskSchema = new mongoose.Schema(
   {
+    userId: { type: Number, required: true },
+    userName: { type: String, required: true },
     taskName: { type: String, required: true },
     documentType: { type: String, required: true },
+    assignTo: {
+      userId: { type: Number, required: true },
+      username: { type: String, required: true },
+    },
     description: { type: String, required: true },
   },
   { timestamps: true }
