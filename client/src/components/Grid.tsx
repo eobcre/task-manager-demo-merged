66d2a5559dd38df8f14b5f7bc3069d3ed6a5db02
Emmy Manning <emswoo@EM.local>
2024-10-04 19:54:16 -0400

    fixed grid style and removed view button

--- client/src/components/Grid.tsx
+++ client/src/components/Grid.tsx
@@ -1,7 +1,9 @@
-import Button from './Button';
+// import Button from './Button';
 
 interface Task {
   _id: string;
+  userId: number;
+  userName: string;
   taskName: string;
   documentType: string;
   createdAt: string;
@@ -15,23 +17,23 @@ const Grid: React.FC<GridProps> = ({ taskData }) => {
   return (
     <div className='border border-slate-500 rounded-md w-full h-auto'>
       {/* header row */}
-      <div className='col-span-4 grid grid-cols-[0.5fr_0.5fr_0.5fr_0.5fr_0.4fr] text-white font-bold bg-purple opacity-90 border-b border-slate-400 p-4'>
+      <div className='col-span-4 grid grid-cols-[0.5fr_0.5fr_0.5fr_0.5fr] text-white font-bold bg-purple rounded-t border-b border-slate-400 opacity-90 p-4'>
         <div>Task</div>
         <div>Assigned By</div>
         <div>Document Type</div>
         <div>Date</div>
-        <div></div>
+        {/* <div></div> */}
       </div>
       {/* data row */}
       {taskData.map((cell) => (
-        <div key={cell._id} className='col-span-4 grid grid-cols-[0.5fr_0.5fr_0.5fr_0.5fr_0.4fr] text-dark border-b border-slate-300 p-4'>
+        <div key={cell._id} className='col-span-4 grid grid-cols-[0.5fr_0.5fr_0.5fr_0.5fr] text-dark bg-white last:rounded-b-md border-b last:border-b-0 border-slate-300 p-4'>
           <div>{cell.taskName}</div>
-          <div>Jane Smith</div>
+          <div>{cell.userName}</div>
           <div>{cell.documentType}</div>
           <div>{new Date(cell.createdAt).toLocaleDateString()}</div>
-          <div>
+          {/* <div>
             <Button buttonName='View' buttonStyle='text-white bg-purple hover:opacity-90 rounded-md px-6 py-1' />
-          </div>
+          </div> */}
         </div>
       ))}
     </div>
