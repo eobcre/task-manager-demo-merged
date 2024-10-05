d335fc5a5676aa3668c72bf016bca4b9bb96a16d
Emmy Manning <emswoo@EM.local>
2024-10-04 20:57:51 -0400

    updated assignTasks, retrieveTasks, created retrieveMember API

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const AssignTask = require('../models/assignTask');
+const pool = require('../db/db');
 
 router.post('/assignTasks', async (req, res) => {
-  const { taskName, documentType, description } = req.body;
+  const { userId, userName, taskName, documentType, assignTo, description, flag } = req.body;
 
   const newTask = new AssignTask({
+    userId,
+    userName,
     taskName,
     documentType,
+    assignTo,
     description,
+    flag,
   });
 
   try {
@@ -19,14 +24,33 @@ router.post('/assignTasks', async (req, res) => {
   }
 });
 
-router.get('/retrieveTasks', async (req, res) => {
+router.post('/retrieveTasks', async (req, res) => {
+  const { userId } = req.body;
+  console.log('userId', userId);
+
   try {
-    const tasks = await AssignTask.find();
-    // console.log('tasks', tasks);
+    const tasks = await AssignTask.find({ 'assignTo.userId': userId });
+    // console.log('tasks', tasks)
+
+    // if (tasks.length === 0) {
+    //   return res.status(404).json({ message: 'No tasks found.' });
+    // }
+
     res.json(tasks);
   } catch (error) {
     console.log('Error retrieving tasks:', error.message);
     res.status(500).json({ message: error.message });
+  }
+});
+
+router.post('/retrieveMembers', async (req, res) => {
+  try {
+    const query = 'SELECT "userId", username FROM login WHERE flag = true';
+    const result = await pool.query(query);
+    res.json(result.rows);
+  } catch (error) {
+    console.error('Error retrieving members:', error.message);
+    res.status(500).json({ message: 'Server error' });
   }
 });
 
