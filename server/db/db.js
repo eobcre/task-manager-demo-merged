d083a6430a1c848e86885080e25417b6fc4f4beb
Emmy Manning <emswoo@EM.local>
2024-10-04 20:10:35 -0400

    db connection

--- server/db/db.js
+++ server/db/db.js
@@ -1,8 +1,11 @@
 const { Pool } = require('pg');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const pool = new Pool({
   user: process.env.POSTGRES_USER,
-  host: 'localhost',
+  host: process.env.POSTGRES_HOST,
   database: process.env.POSTGRES_DB,
   password: process.env.POSTGRES_PASSCODE,
   port: process.env.POSTGRES_PORT,
